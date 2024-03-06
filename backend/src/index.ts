6b8effa64cf00430fccc7f1cd44083fa2d2dcac8
nik132-eng <nikunjrohit10@gmail.com>
2024-03-06 08:56:27 +0530

    update backend and common middleware folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,54 +1,15 @@
-import { Hono } from 'hono';
-import { sign } from 'hono/jwt'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { bookRouter } from './routes/blog';
 
-// Create the main Hono app
-const app = new Hono<{
-	Bindings: {
-		DATABASE_URL: string,
-    JWT_SECRET: string,
-	}
+export const app = new Hono<{
+  Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+  }
 }>();
 
-app.post('/api/v1/signup', async (c) => {
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', bookRouter)
 
-	const body = await c.req.json();
-	try {
-		const user = await prisma.user.create({
-			data: {
-				email: body.email,
-				password: body.password
-			}
-		});
-		const jwt = await sign({ id: user.id }, "secret");
-		return c.json({ jwt });
-	} catch(e) {
-		c.status(403);
-		return c.json({ error: "error while signing up" });
-	}
-})
-
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
-
-app.post('/api/v1/blog', (c) => {
-
-	return c.text('signin route')
-})
-
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
-
-export default app;
+export default app
